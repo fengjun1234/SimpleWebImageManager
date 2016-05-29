69e49af38bdde00f8d544e5569c313774a279f20
fengjun1234 <fj_fengjun@126.com>
2016-05-29 22:10:34 +0800

    创建App模型

---
+++ 异步加载图像/AppInfo.h
@@ -0,0 +1,15 @@
+//
+//  AppInfo.h
+//  异步加载图像
+//
+//  Created by fengjun on 16/5/29.
+//  Copyright © 2016年 fengjun. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppInfo : NSObject
+@property (nonatomic, copy) NSString *name;
+@property (nonatomic, copy) NSString *download;
+@property (nonatomic, copy) NSString *icon;
+@end
---
+++ 异步加载图像/AppInfo.m
@@ -0,0 +1,13 @@
+//
+//  AppInfo.m
+//  异步加载图像
+//
+//  Created by fengjun on 16/5/29.
+//  Copyright © 2016年 fengjun. All rights reserved.
+//
+
+#import "AppInfo.h"
+
+@implementation AppInfo
+
+@end
