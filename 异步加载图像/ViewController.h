a2ad22b215e9c735afaa7bf430cdfc4fb56c30a2
fengjun1234 <fj_fengjun@126.com>
2016-05-29 21:49:32 +0800

    Initial Commit

---
+++ 异步加载图像/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  异步加载图像
+//
+//  Created by fengjun on 16/5/29.
+//  Copyright © 2016年 fengjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 异步加载图像/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  异步加载图像
+//
+//  Created by fengjun on 16/5/29.
+//  Copyright © 2016年 fengjun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
